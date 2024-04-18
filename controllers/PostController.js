0d0d5276d5dbaea84e97f341d9bdfffcce97b6aa
Angelina <loseva-gelya@mail.ru>
2024-04-18 11:45:13 +0300

    added getOne function, return only one post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,10 +2,47 @@ import PostModel from '../models/Post.js';
 
 export const getAll = async (req, res) => {
     try {
-        const posts = await PostModel.find();
+        const posts = await PostModel.find().populate('user').exec();
 
         res.json(posts);
     } catch {
+        console.log(err);
+        res.status(500).json({
+            message : "Не удалось получить статьи",
+        });
+    }
+};
+
+export const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        PostModel.findByIdAndUpdate(
+        {
+            _id: postId,
+        }, 
+        {
+            $inc { viewsCount : 1},
+        },
+        {
+            returnDocumnet : "after",
+        },
+        (err, doc) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    message : "Не удалось вернуть статью",
+                });
+            }
+
+            if (!doc) {
+                return res.status(404).json({
+                    message : "Статья не найдена",
+                })
+            }
+        },
+        );
+    } catch (err) {
         console.log(err);
         res.status(500).json({
             message : "Не удалось получить статьи",
